2d41b988c2c58ddd0a54c1c70a7c9094e06e9df2
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2016-11-05 22:55:52 +0000

    hopefully fixed questiongen

--- questiongame/questionGenerator.js
+++ questiongame/questionGenerator.js
@@ -3,61 +3,65 @@
  */
 
 var sqlite = require("sqlite3").verbose();
-
+var db;
 
 //Fucking async programming why the fuck would anyone ever want to program like this
 
 function QuestionGenerator(callback){
     this.currentQuestionId = null;
     this.currentQuestionAnswer = null;
+    this.currentQuestion = null;
+    this.currentAnswers = null;
     this.onCallback = callback;
-}
 
-QuestionGenerator.prototype.initDatabase = function(callback){
-    var db = new sqlite.Database("../database.db", null, callback);
-};
+    this.initDatabase = function(){
+        db = new sqlite.Database("../database.db", sqlite.OPEN_READONLY, function(err){
+            console.log("halp");
+            if(err){
+                console.error(err.message);
+            }else{
+                this.getQuestion();
+            }
+        }.bind(this));
+    };
 
-QuestionGenerator.prototype.getQuestion = function(){
-
-    var $this = this;
-    var statement = db.prepare("SELECT `id`, `questionText`, `questionAnswer` FROM `questions` ORDER BY RANDOM() LIMIT 1");
-
-    statement.get(function(err, row){
+    this.handleQuestion = function(err, question){
         if(err){
             return console.error("Couldn't get question from db: " + err.message);
         }else{
-            $this.handleQuestion(row);
+            this.currentQuestion = question;
+            this.getAnswers();
         }
-    });
-};
+    };
 
-QuestionGenerator.prototype.getAnswers = function(question){
-    var $this = this;
-    var statement = db.prepare("SELECT `questionAnswers`.`id`, `answerText` FROM `questionAnswers` JOIN `questionQuestionAnswers` ON `questionAnswers`.`id`= `questionQuestionAnswers`.`answerID` WHERE `questionID` = ?", question.id);
+    this.getQuestion = function(){
+        console.log("im trapped in a function");
 
-    statement.all(function(err, rows){
+        var statement = db.prepare("SELECT `id`, `questionText`, `questionAnswer` FROM `questions` ORDER BY RANDOM() LIMIT 1");
+
+        statement.get(this.handleQuestion.bind(this));
+    }
+
+    this.getAnswers = function(){
+
+        var statement = db.prepare("SELECT `questionAnswers`.`id`, `answerText` FROM `questionAnswers` JOIN `questionQuestionAnswers` ON `questionAnswers`.`id`= `questionQuestionAnswers`.`answerID` WHERE `questionID` = ?", this.currentQuestion.id);
+
+        statement.all(this.handleAnswers.bind(this));
+    };
+
+    this.handleAnswers = function(err, rows){
         if(err){
             return console.error("Couldn't get answers from db: " + err.message);
         }else{
-            $this.handleAnswers(question, rows);
+            this.currentAnswers = rows;
+            this.onCallback(this.currentQuestion, rows);
         }
-    });
-};
+    };
 
-QuestionGenerator.prototype.handleQuestion = function(question){
-    var $this = this;
-    this.currentQuestionAnswer = question.questionAnswer;
+    this.verifyQuestion = function(answerID){
+        return (answerID == this.currentQuestionAnswer);
+    };
 
-    $this.getAnswers(question);
-};
-
-QuestionGenerator.prototype.handleAnswers = function(question, answers){
-    var $this = this;
-    $this.onCallback(question, answers)
-};
-
-QuestionGenerator.prototype.verifyQuestion = function(answerID){
-    return (answerID == this.currentQuestionAnswer);
-};
+}
 
 module.exports = QuestionGenerator;
