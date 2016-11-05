e8be5d91733fb700c65f57de93abf58601743f3e
Edsterr <ethandunsmore@hotmail.com>
2016-11-05 18:31:55 +0000

    Update beacons

--- public/javascripts/beacons.js
+++ public/javascripts/beacons.js
@@ -2,24 +2,27 @@
  * Created by Edsterr on 05/11/2016.
  */
 
-//Create test beacons array
-var beacons = [];
-for (var i = 0; i <= 10; i++) {
-    beacons.push(i);
-}
+function beaconGenerator(){
 
-//Init current beacons
-var currentBeacon = 0;
-
-currentBeacon = getBeacon(beacons,currentBeacon);
-
-function getBeacon(listB,currentB){
-    if (currentB === 0){
-        return listB[Math.floor(Math.random() * listB.length)];
+    //Create test beacons array
+    var beacons = [];
+    for (var i = 0; i <= 10; i++) {
+        beacons.push(i);
     }
-    var ranBea = listB[Math.floor(Math.random() * listB.length)];
-    if (ranBea == currentB) {
-        ranBea = getBeacon(listB, ranBea);
+
+    //Init current beacons
+    var currentBeacon = 0;
+
+    currentBeacon = getBeacon(beacons,currentBeacon);
+
+    function getBeacon(listB,currentB) {
+        if (currentB === 0) {
+            return listB[Math.floor(Math.random() * listB.length)];
+        }
+        var ranBea = listB[Math.floor(Math.random() * listB.length)];
+        if (ranBea == currentB) {
+            ranBea = getBeacon(listB, ranBea);
+        }
+        return ranBea;
     }
-    return ranBea;
 }
