5fdc66952c4c4123dcbffbd771755408ea3b67ce
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2016-11-05 15:58:15 +0000

    basic directory structure

---
+++ src/riddlegame/riddleGenerator.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tom on 05/11/16.
+ */
+
+var sqlite = require("sqlite3").verbose();
+var db = new sqlite.Database("../database.db");
+
+var riddleGenerator = new function(){
+
+    function getRiddle(){
+
+    }
+
+    function verifyRiddle(riddleID, answerID){
+
+    }
+}
